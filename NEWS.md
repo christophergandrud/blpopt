ae504287b2ed4874480e18d34f4d6eee0461d348
cgandrud <christopher.gandrud@gmail.com>
2022-12-03 09:04:26 +0100

    first commite

---
+++ NEWS.md
@@ -0,0 +1,3 @@
+# blpopt 0.0.9000
+
+* Basic code passing minimal CRAN checks (without License)
---
+++ R/cate_blp.r
@@ -0,0 +1,60 @@
+#' Estimates the best linear projection of the CATE with respect to a feature
+#' matrix A using cross-fitted DR/AIPW-scores.
+#'
+#' @param cf a *causal_forest* object output from the `causal_forest` function
+#' from the **grf** package.
+#' @param A Matrix of features with the same number of rows as `cf`
+#' @importFrom stats lm
+#' @importFrom lmtest coeftest
+#' @importFrom sandwich vcovHC
+#' @export cate_blp
+
+
+cate_blp <- function(cf, A) {
+
+  # These are the observed outcomes/treatments stored in the GRF object
+  Y <- cf$Y.orig
+  W <- cf$W.orig
+
+  # Step 1: Compute Scores Gamma_i
+  # Note, that we currently rely on the GRF implementation for cross-fitted
+  # nuisance function estimates with regression forests.
+
+  # These are the regression forest estimates of E[Y | X = x] and E[W | X = x]..
+  Y.hat <- cf$Y.hat
+  W.hat <- cf$W.hat
+  # and the localized predictions of the causal forest E[Y_1 - Y_0 | X = x]
+  tau.hat <- cf$predictions
+
+  # Using the relationships
+  # E[Y | X = x] = mu_0(X) + e(X) * tau(X) and
+  # mu_1(X) = mu_0(X) + tau(X) we can compute the GRF implied
+  # estimates of mu_0(X) and mu_1(X) as
+
+  # E[Y | X, W = 0]
+  mu.hat.0 <- Y.hat - W.hat * tau.hat
+  # E[Y | X, W = 1]
+  mu.hat.1 <- Y.hat + (1 - W.hat) * tau.hat
+
+  # DML residual
+  w.res <- W - W.hat
+
+  # DR correction weights
+  weights <- w.res / ((W.hat) * (1 - W.hat))
+  y.res <- Y - (Y.hat + tau.hat * w.res)
+
+  # DR Scores
+  gamma.hat <- y.res * weights + tau.hat
+
+  # Projection of DR scores onto Feature Mat A.
+  blp <- lm(gamma.hat ~ A)
+  # HC3-SE t-tests
+  res <- coeftest(blp, vcov. = vcovHC(blp, type = "HC3"))
+
+  print("CATE-BLP Coefficient Estimates")
+  print(res[, 1])
+  print("HC3 Standard Errors")
+  print(res[, 2])
+
+  return(res)
+}
