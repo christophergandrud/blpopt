84aa9322ab80686e28cd9763815688b9ab2cc423
cgandrud <christopher.gandrud@gmail.com>
2022-12-03 11:40:14 +0100

    add readme and assertions

--- R/cate_blp.r
+++ R/cate_blp.r
@@ -1,16 +1,17 @@
 #' Estimates the best linear projection of the CATE with respect to a feature
 #' matrix A using cross-fitted DR/AIPW-scores.
 #'
-#' @param cf a *causal_forest* object output from the `causal_forest` function
+#' @param cf *causal_forest* object output from the `causal_forest` function
 #' from the **grf** package.
-#' @param A Matrix of features with the same number of rows as `cf`
+#' @param A Matrix of features with the same number of rows as `cf.predictions`
 #' @importFrom stats lm
 #' @importFrom lmtest coeftest
 #' @importFrom sandwich vcovHC
 #' @export cate_blp
 
-
 cate_blp <- function(cf, A) {
+    stopifnot("cf must be causal_forest" = inherits(cf, "causal_forest"))
+    stopifnot("A must be a matrix" = inherits(A, "matrix"))
 
   # These are the observed outcomes/treatments stored in the GRF object
   Y <- cf$Y.orig
@@ -25,6 +26,9 @@ cate_blp <- function(cf, A) {
   W.hat <- cf$W.hat
   # and the localized predictions of the causal forest E[Y_1 - Y_0 | X = x]
   tau.hat <- cf$predictions
+
+  stopifnot("A must have the same number of rows as cf.predictions" =
+                length(A) == length(tau.hat))
 
   # Using the relationships
   # E[Y | X = x] = mu_0(X) + e(X) * tau(X) and
---
+++ README.md
@@ -0,0 +1,30 @@
+
+# blpopt
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+Estimate the best linear projection of the Conditional Average Treatment Effect for approximately optimal targeting
+
+## Installation
+
+You can install the development version of blpopt from [GitHub](https://github.com/) with: 
+
+``` r
+# install.packages("devtools")
+devtools::install_github("christophergandrud/blpopt",
+    auth_token = "ADD TOKEN")
+```
+
+Note: currently blpopt is in a private GitHub repo. You need to 
+add your auth token (if you have access) to download using devtools.
+
+## Example
+
+TO-ADD
+
+``` r
+library(blpopt)
+## basic example code
+```
+
